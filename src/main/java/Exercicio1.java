68e1454c4a970c3fd2c2a0f1b0d05bfe9316d61f
GabrielGuimaraes12 <100451778+GabrielGuimaraes12@users.noreply.github.com>
2022-02-28 18:47:03 -0300

    feat: add exercicio1

---
+++ src/main/java/Exercicio1.java
@@ -0,0 +1,13 @@
+public class Exercicio1 {
+    public static void main(String[] args){
+        int numZumbis1 = 120;
+        int numZumbis2 = 260;
+        int numZumbis3 = 320;
+        int totalZumbis = numZumbis1 + numZumbis2 + numZumbis3;
+
+        System.out.println(totalZumbis);
+        System.out.println(totalZumbis/3);
+    }
+
+
+}

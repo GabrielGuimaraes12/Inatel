403058b21b5e6dd2f03250b2ccb27d74d585c2ce
GabrielGuimaraes12 <100451778+GabrielGuimaraes12@users.noreply.github.com>
2022-03-01 17:02:45 -0300

    feat: add exercicio3

---
+++ src/main/java/Exercicio3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int npa;
+        int np3;
+        System.out.println("Nota da prova: ");
+        npa = entrada.nextInt();
+        if (npa>=60){
+            System.out.println("Aprovado");
+        }
+        else if (npa<30){
+            System.out.println("Reprovado");
+        }
+            else{
+            System.out.println("Nota NP3: ");
+                np3 = entrada.nextInt();
+                if ((np3+npa)/2>=50) {
+
+                    System.out.println("Aprovado");
+                }
+                    else {
+                        System.out.println("Reprovado");
+                    }
+
+        }
+
+
+
+
+    }
+
+
+
+}
